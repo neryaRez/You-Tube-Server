2442c18d02d9c2819153558bb90c08699023a78c
nerya.reznikovich <neryarez@gmail.com>
2025-07-04 18:11:48 +0300

    n

---
+++ middlewares/isAdmin.js
@@ -0,0 +1,8 @@
+// middlewares/isAdmin.js
+module.exports = function (req, res, next) {
+    if (!req.user || !req.user.isAdmin) {
+      return res.status(403).json({ message: "Admin access only" });
+    }
+    next();
+  };
+  
